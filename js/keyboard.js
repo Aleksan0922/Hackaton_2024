f4ee97f18097cb4444a8f147299404bcb25d46c2
cattmem <115585790+cattmem@users.noreply.github.com>
2024-05-16 17:41:53 +0300

    Add files via upload

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,13 +1,46 @@
-var MaskNum = new IMask(
+var testmask = new IMask(
     document.getElementById('test-input'),
     {
-      mask: Number,
-      thousandsSeparator: ' '
+        mask: Number,
+        scale: 5,
+        thousandsSeparator: ' ',
+        radix: ',',
+        mapToRadix: ['.']
     }
 )
 
-function addE(symv) {
-    document.getElementById('test-input').value += symv;
-    MaskNum.updateValue();
-    MaskNum.updateControl();
+function input() {
+    if (document.querySelector('#test-input').value.includes(',')) {
+        document.querySelector('#num-panel-point').style = 'opacity: .5;pointer-events: none;'
+    } else {
+        document.querySelector('#num-panel-point').style = 'opacity: 1;pointer-events: all;'
+    }
+
+    if (document.querySelector('#test-input').value.length == 0) {
+        document.querySelector('#num-panel-erase').style = 'opacity: .5;pointer-events: none;'
+    } else {
+        document.querySelector('#num-panel-erase').style = 'opacity: 1;pointer-events: all;'
+    }
 }
+
+function erase() {
+    document.querySelector('#test-input').value = ''
+    input()
+    testmask.updateValue();
+    testmask.updateControl();
+}
+
+function addE(symv) {
+    document.getElementById('test-input').value += symv
+
+    input()
+
+    if (symv != ',') {
+        testmask.updateValue();
+        testmask.updateControl();
+    }
+}
+
+input()
+
+document.querySelector('#test-input').addEventListener('input', input)
